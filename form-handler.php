92c9c7fedcfa107a15d931f3b5efaea8dd0c430e
kDbee <99341856+kDbee@users.noreply.github.com>
2022-04-06 10:46:51 +0000

    Update form-handler.php

--- form-handler.php
+++ form-handler.php
@@ -1,27 +1,37 @@
-<?php
-    $name = $_POST['name'];
-    $vistor_email = $_POST['email'];
-    $subject = $_POST['subject'];
-    $message = $_POST['message'];
+<?php 
+
+$myemail = 'kwakubondzi6@gmail.com';//<-----Put Your email address here.
+if(empty($_POST['name'])  || 
+   empty($_POST['email']) ||
+   empty($_POST['subject']) ||  
+   empty($_POST['message']))
 
 
-    $email_from = 'info@yourwebsite.com';
+$name = $_POST['name']; 
+$email_address = $_POST['email']; 
+$subject = $_POST['subject']; 
+$message = $_POST['message']; 
 
-    $email_subject = 'New from Submission';
 
-    $email_body = "User Name: $name.\n".
-                    "User Email: $vistor_email.\n".
+
+if( empty($errors))
+{
+	$to = $myemail; 
+	$email_subject = "Contact form submission: $name";
+	$email_body = "Name: $name.\n".
+                    "Email: $email_address.\n".
                     "Subject: $subject.\n".
-                    "User Message: $message.\n";
-    
-    $to = 'kwakubondzi6@gmail.com'
-    
-    $headers = "From: $email_from \r\n";
+                    "Message: $message.\n";
+	
+	$headers = "From: $myemail\n"; 
+	$headers .= "Reply-To: $email_address";
+	
+	mail($to,$email_subject,$email_body,$headers);
+	//redirect to the 'thank you' page
+	header('Location: contact-form-handler.html');
+} 
 
-    $headers .= "Reply-To: $vistor_email \r\n";
 
-    mail( $to,$email_subject,$email_body, $headers);
 
-    header("location: contact.html");
 ?>
 
